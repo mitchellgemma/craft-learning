038760cf7fc3350a1c2f36e9920902cf9daff167
mitchellgemma <mitch@hellobonfire.com>
2022-06-22 14:24:38 -0400

    adds all blog files

--- web/js/app.js
+++ web/js/app.js
@@ -23,39 +23,15 @@ const clickButton = (e) => {
   // if the target does not have an id, find the element with 'checked' id and remove it
   if (!e.target.class) {
 
-    document.getElementById('checked').classList.remove('checked')
-    // give the new element the id 'checked
+    document.querySelector('.checked').classList.remove('checked')
+    // give the new element the class 'checked
     e.target.classList = 'checked'
   }
-  // if user clicks on show even button, displays will be changed accordingly
-  // for (let i = 0; i < oddBoxes.length; i++) {
-  //   if (e.target.className == 'show-even') {
-  //     oddBoxes[i].style.display = 'none'
-  //     evenBoxes[i].style.display = 'flex'
-  //   }
-  //   // for show-odd button, even boxes will be hidden
-  //   else if (e.target.className == 'show-odd') {
-  //     evenBoxes[i].style.display = 'none'
-  //     oddBoxes[i].style.display = 'flex'
 
-  //     // when show all is clicked, all displays will be changed back to normal
-  //   } else if (e.target.className == 'show-all') {
-  //     evenBoxes[i].style.display = 'flex'
-  //     oddBoxes[i].style.display = 'flex'
-  //   }
-  // }
 }
 // Simple function to flip the colors of the nav bar links
 const nav = (e) => {
-  // if home is clicked make home beige and away white
-  if (e.target.id == 'home') {
-    home.style.color = 'rgb(253, 236, 202)'
-    away.style.color = 'white'
-    // if away is clicked make home white and away beige
-  } else if ((e.target.id = 'away')) {
-    home.style.color = 'white'
-    away.style.color = 'rgb(253, 236, 202)'
-  }
+
 }
 
 //========EVENT LISTENERS===================================================================================
@@ -120,7 +96,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 // Only plays once but works with all animations
   ScrollTrigger.batch(cards, {
-    start: "center 85%",
+    start: "top 95%",
 
     onEnter: card => gsap.to(card, {autoAlpha: 1, stagger: 0.1}),
   });
